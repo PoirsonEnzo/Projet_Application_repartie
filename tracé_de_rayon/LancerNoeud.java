4fee10882cd724b735f78ae60ba3c30b7b65a93f
Nurd_Enzo <poirsonenzo@gmail.com>
2026-06-08 15:38:26 +0200

    Ajout de LancerNoeud

---
+++ tracé_de_rayon/LancerNoeud.java
@@ -0,0 +1,23 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class LancerNoeud {
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Usage: java LancerNoeud <adresse_serveur> <nom_service_central>");
+            return;
+        }
+        try{
+            Registry annuaire = LocateRegistry.getRegistry(args[0],1099);
+            ServiceCentral central = (ServiceCentral) annuaire.lookup(args[1]);
+            NoeudDessin noeud = new NoeudDessin();
+            ServiceNoeud servNoeud = (ServiceNoeud) UnicastRemoteObject.exportObject(noeud, 0);
+            central.inscriptionNoeud(servNoeud);
+        } catch (Exception e) {
+            System.err.println("Erreur lors du lancement du nœud :");
+            e.printStackTrace();
+        }
+    }
+}
