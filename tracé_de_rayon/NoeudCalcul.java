28b98232fb3772a8bbcb05a13f2199a753010e70
Martin Gouthier <martin.gouthier1@etu.univ-lorraine.fr>
2026-06-08 15:55:51 +0200

    Fix

--- tracé_de_rayon/NoeudCalcul.java
+++ tracé_de_rayon/NoeudCalcul.java
@@ -5,10 +5,7 @@ import java.rmi.RemoteException;
 
 public class NoeudCalcul implements ServiceNoeud {
 
-    public void compute(Disp fenetre, Scene scene, int x, int y, int l, int h) throws RemoteException {
-        Image image = scene.compute(x,y,l,h);
-        synchronized(fenetre){
-            fenetre.setImage(image,x,y);
-        }
+    public Image compute(Scene scene, int x, int y, int l, int h) throws RemoteException {
+        return scene.compute(x,y,l,h);   
     }
 }
