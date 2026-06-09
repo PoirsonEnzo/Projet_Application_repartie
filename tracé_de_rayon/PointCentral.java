2709d31f4d12aba7a67effda42cbbd8904e01106
Bougnoux Leo <e44214u@etu.univ-lorraine.fr>
2026-06-09 10:38:02 +0200

    suppresion et ajout de noeud occupes

--- tracé_de_rayon/PointCentral.java
+++ tracé_de_rayon/PointCentral.java
@@ -1,23 +1,38 @@
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class PointCentral implements ServiceDistributeurNoeud {
     private List<ServiceNoeud> noeuds = new ArrayList<>();
     private int index = 0;
+
+    
  
     public void inscriptionNoeud(ServiceNoeud noeud) throws RemoteException {
         noeuds.add(noeud);
     }
+    private Set<ServiceNoeud> occupes = new HashSet<>();
 
     public ServiceNoeud getNoeud() throws RemoteException {
-        
-        if (noeuds.isEmpty()){
-            throw new RemoteException("Aucun noeud disponible");
-        } 
+        for (int i = 0; i < noeuds.size(); i++) {
+            ServiceNoeud n = noeuds.get(index);
+            index = (index + 1) % noeuds.size();
+            if (!occupes.contains(n)) {
+                occupes.add(n);
+                return n;
+            }
+        }
+        throw new RemoteException("Aucun noeud disponible");
+    }
 
-        ServiceNoeud n = noeuds.get(index);
-        index = (index + 1) % noeuds.size();
-        return n;
+    public void libererNoeud(ServiceNoeud noeud) throws RemoteException {
+        occupes.remove(noeud);
+    }
+
+    public void suppressionNoeud(ServiceNoeud noeud) throws RemoteException {
+        noeuds.remove(noeud);
+        occupes.remove(noeud);
     }
 }
--- tracé_de_rayon/ServiceDistributeurNoeud.java
+++ tracé_de_rayon/ServiceDistributeurNoeud.java
@@ -4,4 +4,7 @@ import java.rmi.RemoteException;
 public interface ServiceDistributeurNoeud extends Remote {
     void inscriptionNoeud(ServiceNoeud noeud) throws RemoteException;
     ServiceNoeud getNoeud() throws RemoteException;
+
+    void libererNoeud(ServiceNoeud noeud) throws RemoteException;
+    void suppressionNoeud(ServiceNoeud noeud) throws RemoteException;
 }
